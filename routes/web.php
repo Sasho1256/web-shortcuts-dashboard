5e000cd18efb85451ea09fc4ee1fd4c532960c97
Alexander Milachkov <alex@01-hub.com>
2024-11-21 14:19:01 +0200

    Make web.php more readable

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,11 @@ use App\Http\Controllers\LinkController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
 Route::get('links/add/{position}', [LinkController::class, 'add'])->name('links.add');
 Route::post('links/add/{position}', [LinkController::class, 'create'])->name('links.create');
+
 Route::get('links/edit/{link}', [LinkController::class, 'edit'])->name('links.edit');
 Route::put('links/edit/{link}', [LinkController::class, 'update'])->name('links.update');
+
 Route::delete('links/delete/{link}', [LinkController::class, 'delete'])->name('links.delete');
